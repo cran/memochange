e5bf1e13407eef6d8748ece9a497f2efd337f07b
Kai Wenger <kai.wenger@gmx.de>
2020-07-26 22:10:14 +0000

    version 1.1.1

--- inst/doc/Change_in_Mean.R
+++ inst/doc/Change_in_Mean.R
@@ -40,8 +40,8 @@ mean(results_long)
 nasdaq=data.table::fread("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=NASDAQCOM&scale=left&cosd=2006-01-01&coed=2009-01-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2019-11-04&revision_date=2019-11-04&nd=1971-02-05")
 
 ## ---- echo = TRUE, fig.height = 4, fig.width = 7, fig.align = "center"--------
+nasdaq[NASDAQCOM==".", NASDAQCOM := NA]
 nasdaq              <- as.data.frame(nasdaq)
-nasdaq[nasdaq=="."] <- NA
 nasdaq              <- stats::na.omit(nasdaq)
 nasdaq$NASDAQCOM    <- as.numeric(nasdaq$NASDAQCOM)
 nasdaq$DATE=zoo::as.Date(nasdaq$DATE)
